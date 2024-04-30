e81b8846b8fd184bc678436ba8a8da838c1b8df7
Abdelouahed Ouarrar <ouarrar.abdelouahed@gmail.com>
2024-04-30 22:01:00 +0100

    tasks

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,14 +10,12 @@ def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ displays a HTML page with a list of states """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     return render_template('7-states_list.html', states=states)
-
 
 if __name__ == "__main__":
     """ Main Function """
